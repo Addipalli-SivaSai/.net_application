f0079fbad1b441d7fd7cd0ca024b36e10d311d82
Addipalli-SivaSai <addipalli.sivasai@ltts.com>
2024-08-21 16:23:00 +0530

    Create Test.cs

---
+++ Model/Test.cs
@@ -0,0 +1,42 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Movie_Booking.Model
+{
+    public class Test
+    {
+        [Key]
+        public  int movieId{get; set;}
+
+        public  byte[]? movie_poster{get; set;}
+
+        public  string? movieName{get; set;}
+        public  string? RunTime{get; set;}
+     
+       public  string? HeroName{get;set;}
+
+       public  string? HeroineName{get; set;}
+
+       public  string? Director{get; set;}
+
+       public  string? Producer{get; set;}
+
+
+       public  string? musicDirector{get; set;}
+
+       public required string? language{get;set;}
+
+       public  string? movieDescription{get; set;}
+
+[Column(TypeName = "Date")]
+       public  DateTime ReleaseDate{get;set;}
+    
+
+      [Column(TypeName = "Date")]
+       public  DateTime EndDate{get;set;}
+
+   
+    
+
+    }
+}
